7e12e0b0e9fdfa48d0a6804bd25c049d894565fb
adnanasbashi <adnanasbashi@gmail.com>
2026-09-02 12:16:28 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
-// Replace with your active Supabase configuration
+// Replace the old URL with your active Supabase URL
 const SUPABASE_URL = "https://nqwsowkcazkfpqmwmsqw.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_liFbnyZgnxTgNc2cBHuW8w_oLyoFFqO";
 
-// Initialize client
 const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
